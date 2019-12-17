128563747f0ad2baf3f27276ab2f5ef116931ef6
Conor Garity <conorgar@gmail.com>
2019-12-17 15:28:28 -0500

    responsive footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -24,7 +24,9 @@ class Footer extends React.Component {
                             <h5 className='navLink' >Contact</h5>
                         </Link>
                         {/* <h5>FAQ</h5> */}
-                        <a className='other-link' href="https://www.facebook.com/ripplingmedicinals/?__xts__[0]=68.ARDivB9T6kS7kRmyUf0Wahg_CYYPX_RlbPBbXEJYFEjmV3fjlTSi-pdb8Vr9CqgBILJPSU5u5qyYRRS6zGxXN9xQk3Am5-vHsQT0I540HcltqZSg3-E0AO3mKv-38N-65lyw65MA5AXpabwrFv-gGYT8nlvnzj3g8upPParnT1Vle5GBhcO_ObPAsOfYpPFLTzkcE73YCGZuM0-Y6Sl1Th8kCJ98mko5pnCTNUNVFUaYO6y-KjRKtKxhybbbMsnluPSIN8hsN8SJDXJeWxYgcFtTeoRJ-pyvfSOwihzLNUsWrb-vV1T7x8WkFHgJ0ewiaAHDA1h0EOA90hzFdps">Facebook</a>
+                        <div className='other-link'>
+                            <a className='navLink-facebook' href="https://www.facebook.com/ripplingmedicinals/?__xts__[0]=68.ARDivB9T6kS7kRmyUf0Wahg_CYYPX_RlbPBbXEJYFEjmV3fjlTSi-pdb8Vr9CqgBILJPSU5u5qyYRRS6zGxXN9xQk3Am5-vHsQT0I540HcltqZSg3-E0AO3mKv-38N-65lyw65MA5AXpabwrFv-gGYT8nlvnzj3g8upPParnT1Vle5GBhcO_ObPAsOfYpPFLTzkcE73YCGZuM0-Y6Sl1Th8kCJ98mko5pnCTNUNVFUaYO6y-KjRKtKxhybbbMsnluPSIN8hsN8SJDXJeWxYgcFtTeoRJ-pyvfSOwihzLNUsWrb-vV1T7x8WkFHgJ0ewiaAHDA1h0EOA90hzFdps">Facebook</a>
+                        </div>
                         {/* <h5>Instagram</h5> */}
                         {!this.props.isSignedIn && (
 
