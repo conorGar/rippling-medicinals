d334019deb285d0bfb09e976e2ffc6b7cc2a8a11
Conor Garity <conorgar@gmail.com>
2020-01-05 18:35:11 -0500

    visual improvements on product cards

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -4,57 +4,60 @@ import { Link } from "react-router-dom";
 import { apiCall } from '../services/apiService'
 
 class Featured extends React.Component {
-   constructor(props){
-      super(props)
-      this.state = {
-          featuredProducts: []
-      }
-   }
+    constructor(props) {
+        super(props)
+        this.state = {
+            featuredProducts: []
+        }
+    }
 
-   componentDidMount = () =>{
-      this.fetchInfo();
-   }
+    componentDidMount = () => {
+        this.fetchInfo();
+    }
 
-   fetchInfo = async () =>{
-      const response = await apiCall.get(`/product/`)
-      console.log(response.data)
-      this.setState({
-          featuredProducts: response.data
-      })
+    fetchInfo = async () => {
+        const response = await apiCall.get(`/product/`)
+        console.log(response.data)
+        this.setState({
+            featuredProducts: response.data
+        })
 
-   }
+    }
 
-   renderProducts = () =>{
-      const {featuredProducts} = this.state;
+    renderProducts = () => {
+        const { featuredProducts } = this.state;
 
-      return featuredProducts.map(product => {
-         
-          return(
-              <div key={product.id} className='product-container'>
-                  <Link to={`/product/${product.id}`} className='product-link'>
-                      <div className='product-box'>
-                          <img src={product.imgUrl}className='product-image'/>
-                          <h1 className='product-name'>{product.title}</h1>
-                          {/* <h5 className='product-price'>$15.00</h5> */}
+        return featuredProducts.map(product => {
 
-                      </div>
-                  </Link>
+            return (
+                <div key={product.id} className='product-container'>
+                    <Link to={`/product/${product.id}`} className='product-link'>
+                        <div className='product-box'>
+                            <img src={product.imgUrl} className='product-image' />
+                            <div className='product-info-container'>
+                                <h1 className='product-name'>{product.title}</h1>
+                                <h5 className='price-text'>$20.00</h5>
+                            </div>
+                            {/* <h5 className='product-price'>$15.00</h5> */}
 
-              </div>
-          )
-      })
-  }
+                        </div>
+                    </Link>
 
-   render(){
-   return (
-      <div className='featured-holder'>
-         <h1>Featured Products</h1>
-         <div className='featured-list'>
-            {this.renderProducts()}
-         </div>
-      </div>
-   )
-   }
+                </div>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className='featured-holder'>
+                <h1>Featured Products</h1>
+                <div className='featured-list'>
+                    {this.renderProducts()}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Featured;
