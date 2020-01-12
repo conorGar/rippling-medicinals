2f0985585d50673f3031bde134c2016797897c88
Conor Garity <conorgar@gmail.com>
2020-01-12 17:40:23 -0500

    Basic Shop page setup with category bar

--- src/Components/CategoriesRow.js
+++ src/Components/CategoriesRow.js
@@ -6,22 +6,13 @@ function CategoriesRow() {
         <div className='categories-holder'>
             <div className='categories-box'>
                 <img className='categories-image' src='https://s3.amazonaws.com/leafly-s3/content/cannabis-tinctures-101-what-are-they-how-to-make-them-and-how-to/7fV4fF9pTSOhemGxRxyu_tincture-bottles.jpg' />
-                <h2 className='categories-text'>For Aches and Pains</h2>
+                <h2 className='categories-text'>Tea</h2>
             </div>
             <div className='categories-box'>
                 <img className='categories-image' src='https://s3.amazonaws.com/leafly-s3/content/cannabis-tinctures-101-what-are-they-how-to-make-them-and-how-to/7fV4fF9pTSOhemGxRxyu_tincture-bottles.jpg' />
-                <h2 className='categories-text'>For Depression</h2>
+                <h2 className='categories-text'>Tinctures</h2>
             </div>
-            <div className='categories-box'>
-                <img className='categories-image' src='https://s3.amazonaws.com/leafly-s3/content/cannabis-tinctures-101-what-are-they-how-to-make-them-and-how-to/7fV4fF9pTSOhemGxRxyu_tincture-bottles.jpg' />
-
-                <h2 className='categories-text'>For Relaxation</h2>
-            </div>
-            <div className='categories-box'>
-                <img className='categories-image' src='https://s3.amazonaws.com/leafly-s3/content/cannabis-tinctures-101-what-are-they-how-to-make-them-and-how-to/7fV4fF9pTSOhemGxRxyu_tincture-bottles.jpg' />
-
-                <h2 className='categories-text'>To Stop The Voices In My Head</h2>
-            </div>
+            
         </div>
     )
 }
--- src/Pages/Shop/ShopPage.js
+++ src/Pages/Shop/ShopPage.js
@@ -4,20 +4,22 @@ import { Link } from "react-router-dom";
 import { apiCall } from '../../services/apiService'
 import { async } from 'q';
 
-class ShopPage extends React.Component{
-    constructor(props){
+class ShopPage extends React.Component {
+    constructor(props) {
         super(props)
 
         this.state = {
-            allProducts: []
+            allProducts: [],
+            teas: [],
+            currentlyDisplayedProducts: []
         }
     }
 
-    componentDidMount = () =>{
+    componentDidMount = () => {
         this.fetchInfo();
     }
 
-    fetchInfo = async () =>{
+    fetchInfo = async () => {
         const response = await apiCall.get(`/product/`)
         console.log(response.data)
         this.setState({
@@ -26,16 +28,23 @@ class ShopPage extends React.Component{
 
     }
 
-    renderProducts = () =>{
-        const {allProducts} = this.state;
+    fetchTeas = async () => {
+        const res = await apiCall.get('/product/tea')
+        this.setState({
+            teas: res.data
+        })
+    }
+
+    renderProducts = () => {
+        const { allProducts } = this.state;
 
         return allProducts.map(product => {
-           
-            return(
+
+            return (
                 <div key={product.id} className='product-container'>
                     <Link to={`/product/${product.id}`} className='product-link'>
                         <div className='product-box'>
-                            <img src={product.imgUrl} className='product-image'/>
+                            <img src={product.imgUrl} className='product-image' />
                             <h1 className='product-name'>{product.title}</h1>
                             <h5 className='product-price'>$15.00</h5>
 
@@ -47,11 +56,30 @@ class ShopPage extends React.Component{
         })
     }
 
-    render(){
+    renderTeas = () => {
+
+    }
+
+    render() {
         return (
             <div className='shoppage-holder'>
-                
-                {this.renderProducts()}
+
+                <div className='leftside-nav-bar'>
+                    <ul className='options-list'>
+                        Categories:
+                        <li>All</li>
+                        <li>Simple Tinctures</li>
+                        <li>Formula Tinctures</li>
+                        <li>Teas</li>
+
+                    </ul>
+                </div>
+                <div className='product-display-container'>
+                    <h1>- Shop Products -</h1>
+                    <div className='products-holder'>
+                        {this.renderProducts()}
+                    </div>
+                </div>
             </div>
         )
     }
